f73bbf604184a9e022556265d1ef38fc76a8821a
bkellerm <bkellerm@student.ethz.ch>
2018-03-08 12:16:42 +0100

    reorganized some files

--- dist/walking.js
+++ dist/walking.js
@@ -1,12 +1,4 @@
 var Walking = function () {
-	//DEBUG
-	// var line1;
-	// var line2;
-	// var line11;
-	// var lineAlpha;
-	// var lineBeta;
-	// var lineGamma;
-
 	//keywords
 	var _KEYWALKING = "walking";
 	var _KEYSTATIC = "static";
@@ -61,27 +53,6 @@ var Walking = function () {
 			walkDetection();
 		}, false);
 	};
-
-	//Debug
-	// var smoothie = new SmoothieChart({interpolation:'linear'}); //{maxValue:2.5,minValue:-2.5}
-	// line1 = new TimeSeries();
-	// // line2 = new TimeSeries();
-	// smoothie.streamTo(document.getElementById("myCanvas"));
-	// smoothie.addTimeSeries(line1, {strokeStyle:"rgb(0,255,0)"});
-	// // smoothie.addTimeSeries(line2, {strokeStyle:"rgb(255,255,255)"});
-	// var smoothie2 = new SmoothieChart({interpolation:'step'});
-	// line11 = new TimeSeries();
-	// smoothie2.streamTo(document.getElementById("myCanvas2"));
-	// smoothie2.addTimeSeries(line11);
-	// ///
-	// lineAlpha = new TimeSeries();
-	// lineBeta = new TimeSeries();
-	// lineGamma = new TimeSeries();
-	// var smoothie3 = new SmoothieChart();
-	// smoothie3.streamTo(document.getElementById("rotation"));
-	// smoothie3.addTimeSeries(lineAlpha, {strokeStyle:"rgb(255,0,0)"});
-	// smoothie3.addTimeSeries(lineBeta, {strokeStyle:"rgb(0,255,0)"});
-	// smoothie3.addTimeSeries(lineGamma, {strokeStyle:"rgb(0,0,255)"});
 	
 
 	//setter function
@@ -92,17 +63,14 @@ var Walking = function () {
 	}
 
 	//getter functions
-	this.get = function(arg){return defaults[arg]};
-	this.getAll = function(){return defaults};
+	// this.get = function(arg){return defaults[arg]};
+	// this.getAll = function(){return defaults};
 
 
 	var _halfStep = false;
 	function stepDetection(evt) {
 		var yg = evt.acceleration.y; //earth acceleration removed
 		var now = new Date().getTime();
-
-		//DEBUG
-		// line11.append(new Date().getTime(), yg);
 
 		if(_stepsList.length > 0 && _stepsList[0][1] < new Date().getTime() - Math.max(defaults.SPEED_S_W*defaults.WINDOW_S_W, defaults.SPEED_W_S*defaults.WINDOW_W_S) ){
 			_stepsList.shift();
@@ -124,14 +92,6 @@ var Walking = function () {
 				}
 			}
 		}
-
-		//DEBUG
-		// line1.append(new Date().getTime(), stepDetected ? 1 : 0);
-		// var time = _isWalking ? defaults.WINDOW_W_S : defaults.WINDOW_S_W;
-		// var speed = _stepsList.filter(function(t){
-		// 	return t[1] >= now-time;
-		// }).length/time*1000;
-		// $('#stepsPerSecond').text(Math.round(speed*100)/100);
 	}
 	
 	var _timeStateChange = new Date().getTime(); //use to wait window time till next state change can happen
